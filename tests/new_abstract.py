c00bea6a8ff25efd52efb7b886b6c1d2f4177d7f
ruro <ruro.ruro@ya.ru>
2024-03-09 00:21:13 +0300

    add some more tests for Abstract

--- tests/new_abstract.py
+++ tests/new_abstract.py
@@ -5,6 +5,14 @@ with warnings.catch_warnings():
 
     from new_abc import Abstract, abstractmethod
 
+    def assert_cant_instantiate(cls, name):
+        try:
+            cls()
+        except TypeError as ex:
+            assert str(ex).startswith(f"Can't instantiate abstract class {name}")
+        else:
+            assert False
+
     # === Abstract interface classes ===
 
     class Abst(Abstract):
@@ -31,16 +39,9 @@ with warnings.catch_warnings():
     assert hasattr(Impl, "__slots__")
     assert not hasattr(Impl, "register")
 
-    # === Can't initialize abstract class ===
+    # === Can only initialize if all abstract methods are implemented ===
 
-    try:
-        Abst()
-    except TypeError as ex:
-        assert str(ex).startswith("Can't instantiate abstract class Abst")
-    else:
-        assert False
-
-    # === Can initialize if all abstract methods are implemented ===
+    assert_cant_instantiate(Abst, "Abst")
 
     impl = Impl()
     impl.check()
@@ -48,3 +49,33 @@ with warnings.catch_warnings():
 
     assert isinstance(impl, Abst)
     assert isinstance(impl, Abstract)
+
+    # === Adding more abstract methods after the fact works ===
+
+    class ExtraAbst(Abst):
+        @abstractmethod
+        def one_more(self): assert False
+
+    assert_cant_instantiate(ExtraAbst, "ExtraAbst")
+
+    # === Implementing just some of the abstract methods is not sufficient ===
+
+    class IncompleteImpl1(ExtraAbst):
+        def check(self): self.reached1 = True
+
+    class IncompleteImpl2(ExtraAbst):
+        def one_more(self): self.reached2 = True
+
+    assert_cant_instantiate(IncompleteImpl1, "IncompleteImpl1")
+    assert_cant_instantiate(IncompleteImpl2, "IncompleteImpl2")
+
+    # === Implementing all abstract methods is sufficient, also multiple inheritance works ===
+
+    class CompleteImpl(IncompleteImpl1, IncompleteImpl2):
+        pass
+
+    complete = CompleteImpl()
+    complete.check()
+    assert complete.reached1
+    complete.one_more()
+    assert complete.reached2
